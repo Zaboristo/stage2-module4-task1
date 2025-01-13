a302d2aa12407573b4a3d6feb2a4e7ea7d88fc24
Boris Aizenberg <boris_aizenberg@epam.com>
2025-01-13 13:15:13 +0500

    sync

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,11 +1,14 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    private static volatile  ThreadSafeSingleton instance = new ThreadSafeSingleton();
+    private static volatile  ThreadSafeSingleton instance;
 
     private ThreadSafeSingleton(){}
 
     public static synchronized ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            instance = new ThreadSafeSingleton();
+        }
         return instance;
     }
 }
